eea9d633c6ace2814d3949f83a04b26f424f6e51
Sharkman478 <steviehelber@gmail.com>
2023-10-26 21:16:42 -0400

    commiting readme changes

--- README.md
+++ README.md
@@ -3,5 +3,6 @@
 Description
 * We were tasked with creating a code that automatically create a professional ReadMe file through a users input. We had to use nodes in order to create this Hw.
 
-Deployed Repo Link
-* https://sharkman478.github.io/Stevies-ReadMe-Generator/
+Example Video
+* https://drive.google.com/file/d/1YzIDpOcjJC1n665DsKA1gxNRveBobIa4/view
+
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,80 +1,4 @@
-// // TODO: Create a function that returns a license badge based on which license is passed in
-// // If there is no license, return an empty string
-// function renderLicenseBadge(license) {
-//   let badgeURL = '';
-//   if (!license) {
-//     return '';
-//   } else if (license === 'MIT') {
-//     badgeURL = 'https://img.shields.io/badge/license-MIT-blue.svg';
-//   } else if (license === 'Apache 2.0') {
-//     badgeURL = 'https://img.shields.io/badge/license-Apache%202.0-blue.svg';
-//   } else if (license === 'GNU GPLv3') {
-//     badgeURL = 'https://img.shields.io/badge/license-GNU%20GPLv3-blue.svg';
-//   }
-// }
-
-// // TODO: Create a function that returns the license link
-// // If there is no license, return an empty string
-// function renderLicenseLink(license) {
-//   let badgeURL = '';
-//   if (!license) {
-//     return '';
-//   } else if (license === 'MIT') {
-//     badgeURL = 'https://choosealicense.com/licenses/mit/';
-//   } else if (license === 'Apache 2.0') {
-//     badgeURL = 'https://www.apache.org/licenses/LICENSE-2.0.txt';
-//   } else if (license === 'GNU GPLv3') {
-//     badgeURL = 'https://choosealicense.com/licenses/gpl-3.0/';
-//   }
-// }
-
-// // TODO: Create a function that returns the license section of README
-// // If there is no license, return an empty string
-// // function renderLicenseSection(license) {
-// //   let badgeURL = '';
-// //   if (!license) {
-// //     return '';
-// //   } else if (license === 'MIT') {
-// //     badgeURL = 'https://choosealicense.com/licenses/mit/';
-// //   } else if (license === 'Apache 2.0') {
-// //     badgeURL = 'https://www.apache.org/licenses/LICENSE-2.0.txt';
-// //   } else if (license === 'GNU GPLv3') {
-// //     badgeURL = 'https://choosealicense.com/licenses/gpl-3.0/';
-// //   }
-// // }
-
-// // TODO: Create a function to generate markdown for README
-// function generateMarkdown(answers) {
-//   return `# ${answers.title}
-
-//   ## Description
-//   ${answers.description}
-
-//   ## Installation
-//   ${answers.installation}
-
-//   ## Usage
-//   ${answers.usage}
-
-//   ## Contribution
-//   ${answers.contribution}
-
-//   ## Tests
-//   ${answers.tests}
-
-//   ## License
-//   This app is cover under ${answers.license} license.
-
-//   ## Questions
-//   GitHub: [${answers.github}](https://github.com/${answers.github})
-//   Email: ${answers.email}
-// `;
-// }
-
-// module.exports = generateMarkdown;
-
-
-// TODO: Create a function that returns a license badge based on which license is passed in
+// Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
   let badgeURL = '';
@@ -90,7 +14,7 @@ function renderLicenseBadge(license) {
   return badgeURL;
 }
 
-// TODO: Create a function that returns the license link
+// Create a function that returns the license link
 // If there is no license, return an empty string
 function renderLicenseLink(license) {
   let badgeURL = '';
@@ -106,7 +30,7 @@ function renderLicenseLink(license) {
   return badgeURL;
 }
 
-// TODO: Create a function that returns the license section of README
+// Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {
   let badgeURL = '';
@@ -122,10 +46,10 @@ function renderLicenseSection(license) {
   return badgeURL;
 }
 
-// TODO: Create a function to generate markdown for README
+// Create a function to generate markdown for README
 function generateMarkdown(answers) {
   return `# ${answers.title}
-  
+
 ## Description
 ${answers.description}
 
@@ -150,7 +74,7 @@ ${answers.contribution}
 ${answers.tests}
 
 ## License
-This app is covered under the ${answers.license} license. [![License](${renderLicenseBadge(answers.license)})](${renderLicenseLink(answers.license)})
+This app is covered under the ${answers.license} license. ![License](${renderLicenseBadge(answers.license)}) (${renderLicenseLink(answers.license)})
 
 ## Questions
 GitHub: [${answers.github}](https://github.com/${answers.github})
